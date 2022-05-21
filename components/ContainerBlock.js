e47da1c41b6d4689a6228249bd556007f3df4a91
Kishore Nadendla <kishore@Kishores-MacBook-Pro.local>
2022-05-21 07:02:37 -0400

    first commit 4

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -8,7 +8,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
   const router = useRouter();
 
   const meta = {
-    title: "Kishore Nadendla - Engineer, Technical, Creator and Opensource Promoter",
+    title: "Kishore Nadendla",
     description: `I've been developing websites for 5 years straight. Get in touch with me to know more.`,
     image: "/avatar.png",
     type: "website",
