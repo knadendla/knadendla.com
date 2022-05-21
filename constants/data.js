a5c38bb60d90ecb0e13ac8b14dd13608e5c95802
Kishore Nadendla <kishore@Kishores-MacBook-Pro.local>
2022-05-21 06:38:26 -0400

    first commit

---
+++ constants/data.js
@@ -0,0 +1,159 @@
+const userData = {
+  githubUsername: "knadendla",
+  name: "Kishore Nadendla",
+  designation: "Engineering Director",
+  avatarUrl: "/avatar.png",
+  email: "nadendla.kishore@gmail.com",
+  phone: "+91 9587738861",
+  address: "Charlotte, NC, USA.",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "Experienced Engineering Director & Principle Architect with a demonstrated history of working and scaling technical solutions across the enterprise",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "App Transform",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Engineering Director",
+      company: "Cognizant Technology Solutions",
+      year: "2020 - Present",
+      companyLink: "https://www.cognizant.com/",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Engineering Portfolio Lead",
+      company: "Cognizant Technology Solutions",
+      year: "2016 - 2020",
+      companyLink: "https://www.cognizant.com/",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Engagement Lead",
+      company: "Cognizant Technology Solutions",
+      year: "2014 - 2016",
+      companyLink: "https://www.cognizant.com/",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Architect",
+      company: "Cognizant Technology Solutions",
+      year: "2011 - 2014",
+      companyLink: "https://www.cognizant.com/",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Techical Lead",
+      company: "Cognizant Technology Solutions",
+      year: "2009 - 2011",
+      companyLink: "https://www.cognizant.com/",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Senior Developer",
+      company: "Cognizant Technology Solutions",
+      year: "2007 - 2009",
+      companyLink: "https://www.cognizant.com/",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Developer",
+      company: "Cognizant Technology Solutions",
+      year: "2004",
+      companyLink: "https://www.cognizant.com/",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Developer",
+      company: "IBS Software",
+      year: "2003",
+      companyLink: "",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Post Graduate",
+      company: "Acharya Nagarjuna University, Andhra Pradesh.",
+      year: "2000 - 2003",
+      companyLink: "https://www.nagarjunauniversity.ac.in/indexanu.html",
+      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+    },
+    {
+      title: "Graduation",
+      company: "Acharya Nagarjuna University, Andhra Pradesh.",
+      year: "1997 - 2000",
+      companyLink: "https://www.nagarjunauniversity.ac.in/indexanu.html",
+      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
+    },
+    {
+      title: "Intermediate",
+      company: "Board of Intermediate Eduction, Andhra Pradesh.",
+      year: "1995 - 1997",
+      companyLink: "https://upes.co.in",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+    {
+      title: "High School",
+      company: "Board of Secondary Eduction, Andhra Pradesh.",
+      year: "1995",
+      companyLink: "https://upes.co.in",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://standardresume.co/r/bLwdAQiwzm7J5D4QA1n7v",
+  socialLinks: {
+    instagram: "https://instagram.com/knadendla",
+    twitter: "https://twitter.com/knadendla",
+    linkedin: "https://www.linkedin.com/in/kishore-nadendla-68a78062/",
+    github: "https://github.com/knadendla",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Projects from "../components/Projects";
+
+export default function projects() {
+  return (
+    <ContainerBlock title="Projects - Kishore Nadendla">
+      <Projects />
+    </ContainerBlock>
+  );
+}
