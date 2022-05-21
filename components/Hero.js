aa60b90d5176fdb239f9191c374e7b66d8501a69
Kishore Nadendla <kishore@Kishores-MacBook-Pro.local>
2022-05-21 06:46:47 -0400

    first commit 1

--- components/Hero.js
+++ components/Hero.js
@@ -13,17 +13,17 @@ export default function Hero() {
         <RoughNotationGroup show={true}>
           <RainbowHighlight color={colors[0]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-            Engineer.
+            Father.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[1]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-            Technologist.
+            Husband.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[2]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Husband & Father.
+              Engineer.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[3]}>
